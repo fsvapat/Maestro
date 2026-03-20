426bce9a603fd03122eb1bd303da16cff9143fe5
Valentyn Patsera <valentyn.patsera@fullscript.com>
2026-03-19 21:00:55 -0400

    feat: add interactive cli

--- maestro-cli/src/main/java/maestro/cli/App.kt
+++ maestro-cli/src/main/java/maestro/cli/App.kt
@@ -28,6 +28,7 @@ import maestro.cli.command.CheckSyntaxCommand
 import maestro.cli.command.CloudCommand
 import maestro.cli.command.DownloadSamplesCommand
 import maestro.cli.command.DriverCommand
+import maestro.cli.command.InteractCommand
 import maestro.cli.command.ListCloudDevicesCommand
 import maestro.cli.command.ListDevicesCommand
 import maestro.cli.command.LoginCommand
@@ -73,6 +74,7 @@ import kotlin.system.exitProcess
         CheckSyntaxCommand::class,
         DriverCommand::class,
         McpCommand::class,
+        InteractCommand::class,
     ]
 )
 class App {
